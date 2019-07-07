8db794a02e6ddd993311e1889653f5c00c7bf003
Archzest <33100644+mttorres@users.noreply.github.com>
2019-07-07 20:55:20 -0300

    OpenMP(forma alternativa): removendo print caso formos gerar graficos denovo

--- lpp_trab2/openmp/produtoescalar.c
+++ lpp_trab2/openmp/produtoescalar.c
@@ -13,18 +13,27 @@ int main(int argc, char* argv[]){
 		b[i] = i*2.0;
 	}
 	sum = 0;
+	
 	int totalthreads = omp_get_max_threads();
+	
+	//para debug
+	//printf("total threads: %d \n",totalthreads);
+	
 	double bancodasthreads[totalthreads]; 
 
 	//versao indexada pelo rank
 	#pragma omp parallel for 
 	for(i = 0; i<n;i++){
+		//para debug
+		//printf("(%d): %d  \n",omp_get_thread_num(),i );
 		bancodasthreads[omp_get_thread_num()] =  bancodasthreads[omp_get_thread_num()] +  (a[i] * b[i]);
 	}
 	// agora percorre o "banco" e devolve o resultado
 	for(i = 0 ; i < totalthreads; i++ ){
 		sum = sum+ bancodasthreads[i];
 	}
-	printf("sum = %f\n",sum);
+
+	//removendo para agilizar o teste (se formos rodar varias vezes)
+	//printf("sum = %f\n",sum);
 
 }
