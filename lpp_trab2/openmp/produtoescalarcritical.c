8354753a238c6f7a477f8688e5d08d6aa5899e46
Archzest <33100644+mttorres@users.noreply.github.com>
2019-07-07 23:26:40 -0300

    OPENMP: Versão CRITICAL que acaba com condição de corrida

--- lpp_trab2/openmp/produtoescalarcritical.c
+++ lpp_trab2/openmp/produtoescalarcritical.c
@@ -5,6 +5,7 @@
 // trabalho final parte 1...?  paraleelizar e IMPEDIR CONDIÇÃO DE CORRIDA DE 3 FORMAS DIFERENTES
 int main(int argc, char* argv[]){
 	double sum;
+	double valor;
 	double a[256], b[256];
 	int n= 256,i;
 	
@@ -13,26 +14,26 @@ int main(int argc, char* argv[]){
 		b[i] = i*2.0;
 	}
 	sum = 0;
-
-
 	// condição de corrida aqui
 	// quem fizer com critical DENTRO DO LOOP vai perder ponto!: temporaria e critical fora do loop
-		// com tempo e fora do loop
+		// com temp e fora do loop
 		// reduce
 		// temporario indexado pelo rank da thread temp[rank]?
-	#pragma omp parallel
+
+	// ISSO AQUI TA MUITO ERRADO (ele ta percorrendo o vetor INTEIRO 4 vezes (o numero de threads vezes))
+	#pragma omp parallel shared(a,b,sum) private(valor,i)
 	{
 		double temp = 0;
-		#pragma omp parallel for
+		#pragma omp for
 		for(i = 0; i<n;i++){
-			temp = temp + a[i]*b[i];
+			valor = a[i]*b[i];
+			temp = temp + valor;
+			//printf("Thread%d: %f (%d) \n",omp_get_thread_num(),temp,i);
 		}
 		#pragma omp critical
 		sum = sum + temp;
 
 	}
-	
-
 	printf("sum = %f\n",sum);
 
 }
